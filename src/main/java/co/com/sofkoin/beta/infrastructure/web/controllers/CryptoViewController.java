378b3cb085ee1c328b771e22f372269752e4b69d
Katerin Calderon <kccc0713@gmail.com>
2022-09-24 15:34:35 -0500

    add web controllers

---
+++ src/main/java/co/com/sofkoin/beta/infrastructure/web/controllers/CryptoViewController.java
@@ -0,0 +1,28 @@
+package co.com.sofkoin.beta.infrastructure.web.controllers;
+
+import co.com.sofkoin.beta.application.gateways.rest.RestCryptoResponse;
+import co.com.sofkoin.beta.application.usecases.GetCryptoPriceUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("view/cryptos")
+public class CryptoViewController {
+
+    private GetCryptoPriceUseCase getCryptoPriceUseCase;
+
+    @GetMapping("/price")
+    public ResponseEntity<Mono<RestCryptoResponse>> getCryptosPrice() {
+        return new ResponseEntity<>(
+                this.getCryptoPriceUseCase.get(),
+                HttpStatus.CREATED
+        );
+    }
+
+}
---
+++ src/main/java/co/com/sofkoin/beta/infrastructure/web/controllers/MarketViewController.java
@@ -0,0 +1,41 @@
+package co.com.sofkoin.beta.infrastructure.web.controllers;
+
+import co.com.sofkoin.beta.application.commons.views.MarketView;
+import co.com.sofkoin.beta.application.gateways.rest.RestCryptoResponse;
+import co.com.sofkoin.beta.application.usecases.FindAllMarketsUseCase;
+import co.com.sofkoin.beta.application.usecases.FindMarketByIdUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("view/market")
+public class MarketViewController {
+
+    private FindMarketByIdUseCase findByMarketByIdUseCase;
+    private FindAllMarketsUseCase findAllMarketsUseCase;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Mono<MarketView>> getMarketById(@PathVariable String id) {
+        return new ResponseEntity<>(
+                this.findByMarketByIdUseCase.apply(Mono.just(id)),
+                HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Flux<MarketView>> getAllMarkets() {
+        return new ResponseEntity<>(
+                this.findAllMarketsUseCase.get(),
+                HttpStatus.CREATED
+        );
+    }
+
+}
